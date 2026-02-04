88bc81b121abd2dfb20a04aa2259ae9342750274
RojanShrestha1 <shrestarojan099@gmail.com>
2026-02-04 20:01:13 +0545

    towers of hannoi code

---
+++ tower.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void hanoi(int n, char from, char to, char helper) {
+    if (n == 1) {
+        cout << "Move disk 1 from " << from << " to " << to << endl;
+        return;
+    }
+
+    hanoi(n - 1, from, helper, to);            
+    cout << "Move disk " << n << " from " << from << " to " << to << endl; 
+}
+
+int main() {
+    int n;
+    cout << "Enter number of disks: ";
+    cin >> n;
+
+    hanoi(n, 'A', 'C', 'B');
+    return 0;
+}
